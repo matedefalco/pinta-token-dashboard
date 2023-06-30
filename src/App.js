9ea06d3dd30bbdacdbdb8c5253570ae0680b2785
Suka <mateodefalco1@gmail.com>
2023-06-30 18:58:21 -0300

    Arreglos Infura

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Grid,
-  theme,
-  Text,
-} from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { ChakraProvider, Box, Grid, theme, Text } from '@chakra-ui/react';
 import Web3 from 'web3';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import contractABI from './contractABI';
 
-async function getWallets(contractAddress) {
-  const web3 = new Web3('https://mainnet.infura.io/v3/b383710d86db413d8e6909a08b807754');
+const contractAddress = '0x9Ae69fDfF2FA97e34B680752D8E70dfD529Ea6ca';
+
+async function getWallets() {
+  const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/b383710d86db413d8e6909a08b807754'));
 
   // Obtén la instancia del contrato
   const contract = new web3.eth.Contract(contractABI, contractAddress);
@@ -25,13 +21,11 @@ async function getWallets(contractAddress) {
   return walletList.map((event) => event.returnValues.from);
 }
 
-
 function App() {
   const [wallets, setWallets] = useState([]);
 
   useEffect(() => {
-    const contractAddress = '0x...'; // Aquí debes proporcionar la dirección de tu contrato
-    getWallets(contractAddress)
+    getWallets()
       .then((result) => setWallets(result))
       .catch((error) => console.error(error));
   }, []);
@@ -44,14 +38,14 @@ function App() {
         </Grid>
         <Text>
           {wallets.length > 0 ? (
-              <ul>
-                {wallets.map((wallet) => (
-                  <li key={wallet}>{wallet}</li>
-                ))}
-              </ul>
-            ) : (
-              <p>No se encontraron wallets.</p>
-            )}
+            <ul>
+              {wallets.map((wallet) => (
+                <li key={wallet}>{wallet}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>No se encontraron wallets.</p>
+          )}
         </Text>
       </Box>
     </ChakraProvider>
