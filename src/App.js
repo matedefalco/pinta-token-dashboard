bb80b1a8f452c498c35350e74b1d0cf79c0175b9
Suka <mateodefalco1@gmail.com>
2023-07-03 14:46:55 -0300

    App casi terminada

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { ChakraProvider, Box, Grid, theme, Text } from '@chakra-ui/react';
-import Web3 from 'web3';
+import React, { useState } from 'react';
+import { ChakraProvider, Box, Grid, theme, Text, Tag, Heading, Input, Button, Stack } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import contractABI from './contractABI';
-import { ethers } from "ethers";
+import { ethers } from 'ethers';
 
-// Dirección del contrato en la red de Polygon
-const contractAddress = '0x9Ae69fDfF2FA97e34B680752D8E70dfD529Ea6ca';
-
-// URL del proveedor de Infura para la red de Polygon
-const providerURL = `https://polygon-mainnet.g.alchemy.com/v2/OQmRfTFI5vVbN6oYc39XAkvB6FiW8hFa`;
-
-// Crear una instancia de Web3 utilizando el proveedor de Infura
-const provider = new ethers.providers.JsonRpcProvider(providerURL);
-
-async function getWallets() {
-
-  // The Contract object
-const pntContract = new ethers.Contract(contractAddress, contractABI, provider);
-
-
-  const balance = await provider.getBalance(contractAddress)
-
- return balance;
-}
+const providerURL = 'https://polygon-mainnet.g.alchemy.com/v2/OQmRfTFI5vVbN6oYc39XAkvB6FiW8hFa';
+const provider = new ethers.JsonRpcProvider(providerURL);
 
 function App() {
-  const [wallets, setWallets] = useState([]);
-  
-  useEffect(() => {
-    // Al cargar el componente, obtener las wallets llamando a la función getWallets
-    getWallets()
-    .then((result) => setWallets(result))
-    .catch((error) => console.error(error));
-  }, []);
-  console.log("Suka ~ file: App.js:32 ~ wallets:", wallets)
+  const [balance, setBalance] = useState(null);
+  const [wallet, setWallet] = useState('');
+  const [contract, setContract] = useState('');
+
+  async function getBalance() {
+    if (!contract || !wallet) return;
+
+    try {
+      const tokenContract = new ethers.Contract(contract, contractABI, provider);
+      const walletBalance = Number(await tokenContract.balanceOf(wallet));
+      setBalance(walletBalance);
+      console.log('Suka', walletBalance);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+    
+
+  function handleButtonClick() {
+    getBalance();
+    console.log('Suka', wallet);
+    console.log('Suka', contract);
+    console.log('Suka', balance);
+  }
 
   return (
     <ChakraProvider theme={theme}>
@@ -42,17 +39,20 @@ function App() {
         <Grid p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
         </Grid>
+        <Heading as="h1" noOfLines={1}>
+          Wallet balance
+        </Heading>
         <Text>
-          {wallets.length > 0 ? (
-            <ul>
-              {wallets.map((wallet) => (
-                <li key={wallet}>{wallet}</li>
-              ))}
-            </ul>
-          ) : (
-            <p>No se encontraron wallets.</p>
-          )}
+          Type in the address and the contract, and it will retrieve the wallet's token balance (Only Polygon)
         </Text>
+        <Stack spacing={2} p={8}>
+          <Input placeholder="Wallet" onChange={(e) => setWallet(e.target.value)} />
+          <Input placeholder="Token Contract" onChange={(e) => setContract(e.target.value)} />
+        </Stack>
+        <Button onClick={handleButtonClick}>Confirm</Button>
+        {balance !== null && (
+          <Tag>{balance}</Tag>
+        )}
       </Box>
     </ChakraProvider>
   );
